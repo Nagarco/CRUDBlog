21dff503af121b91f8db652be4f7312e82d4d5b5
Naggarco96 <omarrmahmood@gmail.com>
2023-03-18 14:01:58 +0200

    Add initial code for Node.js and Express CRUD application with MongoDB database

---
+++ controllers/blogController.js
@@ -0,0 +1,41 @@
+const Blog = require('../models/blog');
+
+
+const blog_index = (req, res)=>{
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => res.render('blogs/index', { title: 'All Blogs', blogs: result }))
+        .catch((err) => console.log(err));
+}
+
+const blog_details = (req, res) =>{
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result) => res.render('blogs/details', { title: 'Blog Details', blog: result }))
+        .catch((err) => res.status(404).render('404', { title: '404' }));
+}
+
+const blog_create_get = (req, res)=>{
+    res.render('blogs/create', { title: 'New' });
+}
+
+const blog_create_post = (req, res) =>{
+    const blog = new Blog(req.body);
+    blog.save().then(() => res.redirect('/blogs')).catch((err) => console.log(err));
+} 
+
+const blog_delete = (req, res) =>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+    .then(() => {
+        res.json({redirect: '/blogs'});
+    })
+    .catch(err=> console.log(err));
+}
+
+module.exports = {
+    blog_index,
+    blog_details,
+    blog_create_get,
+    blog_create_post,
+    blog_delete
+}
